648b7b8195b6ef2306ed8d2ea0ece58d35189c89
Phuc Proton <phuc.le.it96@gmail.com>
2024-06-05 02:04:07 +0700

    First Portfolio Project deployed on www.phucproton.me using NextJS and new web stacks

---
+++ data/index.ts
@@ -0,0 +1,217 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I work closely with clients, encouraging open and honest communication.",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Flexible for time zone calls and remote supports.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "Interested in Python and JS",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for Web development and Data Analysis.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently practicing a NextJS and TailwindCSS stack for my projects.",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "3D Solar System Planets to Explore",
+    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
+    img: "/p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "https://github.com/phucleit96",
+  },
+  {
+    id: 2,
+    title: "Yoom - Video Conferencing App",
+    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+    img: "/p2.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "https://github.com/phucleit96",
+  },
+  {
+    id: 3,
+    title: "AI Image SaaS - Canva Application",
+    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "https://github.com/phucleit96",
+  },
+  {
+    id: 4,
+    title: "Animated Apple Iphone 3D Website",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "https://github.com/phucleit96",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Working with Phuc was a delight. His professionalism, responsiveness, and commitment to excellence shone through the entire project. Phuc's genuine passion for development is truly inspiring.  Need to elevate your website and brand? Look no further than Phuc - he's the perfect collaborator!",
+    name: "Lionel Messi",
+    title: "Legendary Footballer",
+  },
+  {
+    quote:
+      "Phuc made our project a breeze. His expertise, efficiency, and unwavering focus on quality were impressive. His infectious passion for development kept the project exciting.  Looking to take your website and brand to the next level? Partner with Phuc - he's a game-changer!",
+    name: "John Doe",
+    title: "Director of ABC Technologies",
+  },
+  {
+    quote:
+      "Phuc's professionalism and dedication ensured our project ran smoothly. His promptness and clear communication kept us informed every step of the way. Phuc's genuine passion for development was contagious, turning complex tasks into engaging experiences.  Ready to unlock the full potential of your website and brand? Phuc's expertise is the key!",
+    name: "Kylian Mbappe",
+    title: "French Superstar Footballer",
+  },
+  {
+    quote:
+      "Phuc's expertise turned our website vision into reality. His professionalism, responsiveness, and results-oriented approach kept the project on track.  His passion for development fueled creative solutions and exceeded our expectations.  To achieve your website and branding goals, Phuc is the perfect partner!",
+    name: "Michael Jackson",
+    title: "Tech Lead at XYZ",
+  },
+  {
+    quote:
+      "Working with Phuc was a masterclass in collaboration.  His sharp mind, proactive approach, and unwavering commitment to results were a winning combination.  His genuine enthusiasm is a force multiplier, elevating every project he touches.  Need a partner to propel your website and brand forward?  Look no further than Phuc!",
+    name: "Jackson Wang",
+    title: "Director of OOP Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Technical Support Engineer",
+    desc: "Assisted in the development of a leading load testing product and provided technical support to customers.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Web Dev - Python and Javascript",
+    desc: "Learned web development from scratch and built several projects to enhance my skills and knowledge.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Wordpress, classical front-end dev and SEO skills",
+    desc: "Deployed and maintained several WooCommerce - Wordpress websites and worked on various front-end projects (classical HTML CSS JS).",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Looking for a chance to enhance either fullstack skills or data analysis",
+    desc: "Developed and maintained user-facing features using modern frontend technologies (React, NextJS, TailwindCSS) and Python frameworks (Django, FastAPI)",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
